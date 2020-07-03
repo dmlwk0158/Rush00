444779364d6a5f0a35e97d83cc36bf91797e67bc
Sujin Jeon <sujeon@c1r6s6.42seoul.kr>
2020-07-03 22:50:20 +0900

    ex00

---
+++ ex00/rush03.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush03.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sujeon </var/mail/sujeon>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/03 22:35:25 by sujeon            #+#    #+#             */
+/*   Updated: 2020/07/03 22:48:48 by sujeon           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int rush03(int x, int y){
+	int i=0;
+	while(y-i){
+		int j=0;
+		while(x-j){
+			if(i==0 || i==(y-1){
+				if (j==0) ft_putchar('A');
+				else if (j==(x-1)) ft_putchar('C');
+				else ft_putchar('B');
+				}
+			else{
+				if(j==0 || j==(x-1)) ft_putchar('B');
+				else ft_putchar('B');
+				}
+			j++
+			}
+		write(1,"\n",1)
+		i++	
+		}
+	return 42;
+}
+
+		
+
